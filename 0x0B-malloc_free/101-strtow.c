a3c0f7e2e47013533a229f0ff04d6013716d5518
DanielFouad <danielfouad8@gmail.com>
2023-11-20 06:51:34 -0800

    malloc free

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,4 +1,4 @@
-#include <stdlib.h>
+#include "main.h"
 
 /**
  * print_tab - Prints an array of string
@@ -16,22 +16,3 @@ void print_tab(char **tab)
 	}
 }
 
-/**
- * main - check the code for ALX School students.
- *
- * Return: 1 if an error occurred, 0 otherwise
-*/
-
-int main(void)
-{
-	char **tab;
-
-	tab = strtow("      ALX School         #cisfun      ");
-	if (tab == NULL)
-	{
-		printf("Failed\n");
-		return (1);
-	}
-	print_tab(tab);
-	return (0);
-}
